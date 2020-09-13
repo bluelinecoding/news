b51cfb44b43703378c8e9ef85ab788066e583e7c
bluelinecoding <tom@blueline-coding.co.uk>
2020-09-13 14:03:32 +0100

    Added basic feed caching

--- feeds/funcs.go
+++ feeds/funcs.go
@@ -1,8 +1,18 @@
 package feeds
 
 import (
+	"time"
+
 	"github.com/mmcdole/gofeed"
+	"github.com/patrickmn/go-cache"
 )
+
+var Cache = cache.New(5*time.Minute, 5*time.Minute)
+
+func setCache(key string, v interface{}) bool {
+	Cache.Set(key, v, cache.DefaultExpiration)
+	return true
+}
 
 func NewParser() *gofeed.Parser {
 	fp := gofeed.NewParser()
@@ -11,10 +21,19 @@ func NewParser() *gofeed.Parser {
 }
 
 func GetFeed(url string, parser *gofeed.Parser) ([]*gofeed.Item, error) {
+	cached, found := Cache.Get(url)
+	if found {
+		items := cached.([]*gofeed.Item)
+		return items, nil
+	}
+
 	feed, err := parser.ParseURL(url)
 	if err != nil {
 		return nil, err
 	}
+
+	// TODO: find out the correct cache time, based on feed time to live details
+	setCache(url, feed.Items)
 
 	return feed.Items, nil
 }
